38903444d4e81179952a6dfb3d07568179931de2
ZAINHANIF <www.zainhanif73@gmail.com>
2022-06-06 11:10:10 +0500

    modify static

--- myapp/settings.py
+++ myapp/settings.py
@@ -83,7 +83,6 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
@@ -121,5 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/images/'
+
+STATIC_FILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 django_heroku.settings(locals())
